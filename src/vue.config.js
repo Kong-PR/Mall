8d60c96b2fbdd5dbeb018ecb293b78a22f02391f
kong <376634352@qq.com>
2021-03-03 15:40:32 +0800

    add init file and create some files

---
+++ src/vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    configureWebpack: {
+        resolve: {
+            alias: {
+                // 默认@对应src
+                'assets': '@/assets',
+                'common': '@/common',
+                'network': '@/network',
+                'components': '@/components',
+                'views': '@/views',
+            }
+        }
+    }
+}
