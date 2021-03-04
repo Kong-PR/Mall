9f77f737cb756783162cab566364ed40ffe6c3e7
kong <376634352@qq.com>
2021-03-04 11:05:49 +0800

    成功运行

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,14 @@
 module.exports = {
-    configureWebpack: {
-        resolve: {
-            alias: {
-                // 默认@对应src
-                'assets': '@/assets',
-                'common': '@/common',
-                'network': '@/network',
-                'components': '@/components',
-                'views': '@/views',
-            }
-        }
-    },
-    lintOnSave: false
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views',
+      }
+    }
+  }
 }
+
