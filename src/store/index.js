d854ce2e3309e63fb3b747f83fbd360d9f9e9c0b
kong <376634352@qq.com>
2021-03-18 15:49:48 +0800

    使用mapGetters优化购物车计算属性

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,7 @@ import Vuex from "vuex"
 
 import mutations from "./mutations"
 import actions from "./actions"
+import getters from "./getters"
 // 安装插件
 Vue.use(Vuex)
 // 创建使用对象
@@ -12,7 +13,8 @@ const store = new Vuex.Store({
     cartList: []
   },
   mutations,
-  actions
+  actions,
+  getters
 
 
 })
--- src/views/shopcart/Shopcart.vue
+++ src/views/shopcart/Shopcart.vue
@@ -1,13 +1,29 @@
 <template>
-  <div><h2>购物车</h2></div>
+  <div class="cart">
+    <nav-bar class="nav-bar">
+      <div slot="center">购物车({{ cartLength }})</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
-	export default {
-		name: "Shopcart"
-	}
+import NavBar from "components/common/navbar/NavBar";
+import { mapGetters } from "vuex";
+export default {
+  name: "Shopcart",
+  components: {
+    NavBar,
+  },
+  computed: {
+    ...mapGetters(["cartLength"]),
+    // 也可以用对象属性来更改名字
+  },
+};
 </script>
 
 <style scoped>
-
+.nav-bar {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
